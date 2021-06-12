506766cca4a4958622c7eadf14efbe5819565b63
Matthew Lau <lau.cy.matthew@gmail.com>
2021-06-12 18:12:56 +0100

    Added requirements.txt

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return f'Hello {os.getenv("APP_NAME")}'
-
 
 @app.route("/ping")
 def ping():
     return "pong!"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
