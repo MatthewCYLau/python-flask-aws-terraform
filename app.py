91b29e557d9ecfd482a08c89cc024df809404cf0
Matthew Lau <lau.cy.matthew@gmail.com>
2021-06-12 22:06:59 +0100

    Add black formatter

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/ping")
 def ping():
+
     return "pong!"
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=True)
