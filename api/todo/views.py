da8d7b3c690cdbaa3a91f5bcbf8db221c130035e
Matthew Lau <matthew.lau@matthews-mbp.home>
2021-06-16 14:50:14 +0100

    Add requests library

--- api/todo/views.py
+++ api/todo/views.py
@@ -1,6 +1,14 @@
 from api import app
 from api.todo.models import Todo
+import requests
+from flask import jsonify
 from api import db
+
+
+todos = [
+    {"id": 1, "name": "foo", "description": "bar"},
+    {"id": 2, "name": "fooo", "description": "baar"},
+]
 
 
 @app.route("/ping")
@@ -12,18 +20,20 @@ def ping():
 @app.route("/todos", methods=["GET"])
 def list_todos():
 
-    todos = Todo.query.all()
+    # todos = Todo.query.all()
+    res = requests.get("https://jsonplaceholder.typicode.com/posts")
 
-    for todo in todos:
-        print(todo)
-
-    return "List todos"
+    if res.status_code == 200:
+        return jsonify(res.json()), 200
+    else:
+        return "Error fetching data", 500
 
 
 @app.route("/todos", methods=["POST"])
 def add_todo():
-    todo = Todo(id=1, name="foo", description="bar", completed=False)
-    db.session.add(todo)
-    db.session.commit()
+    # todo = Todo(id=1, name="foo", description="bar", completed=False)
+    # db.session.add(todo)
+    # db.session.commit()
 
-    return "Added todo"
+    data = todos[0]
+    return jsonify(data), 201
