a2377279e05a83f96b179328c8ee05724c972e30
Matthew Lau <matthew.lau@Matthews-MacBook-Pro.local>
2021-06-16 17:39:54 +0100

    Add env config

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,11 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 # when running app inside container, connect to @db:5432
-app.config[
-    "SQLALCHEMY_DATABASE_URI"
-] = "postgresql://postgres:password@localhost:5432/flaskdb"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 db = SQLAlchemy(app)
 
